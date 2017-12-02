7cd8f44e030ed8a69709cd14739684e9d361e4a3
mgracanin <mijo.gracanin@gmail.com>
2017-12-02 14:56:30 +0100

    Initial Commit

---
+++ AppStoreReviews/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppStoreReviews
+//
+//  Created by Mijo Gracanin on 02/12/2017.
+//  Copyright © 2017 Mijo Gracanin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
